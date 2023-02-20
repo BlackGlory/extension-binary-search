c4266e95d2587cd8db66551d981fde70cf17a33a
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-02-20 20:20:50 +0800

    refactor: rename

--- src/background/storage.ts
+++ src/background/storage.ts
@@ -1,19 +1,19 @@
 import { IExtension, IStorage, StorageItemKey } from '@src/contract'
 import { LocalStorage } from 'extra-webextension'
 
-const localStorage = new LocalStorage<IStorage>()
+const storage = new LocalStorage<IStorage>()
 
 export async function initStorage(): Promise<void> {
-  await localStorage.setItem(StorageItemKey.ExcludedExtensions, [])
+  await storage.setItem(StorageItemKey.ExcludedExtensions, [])
 }
 
 export async function getExcludedExtensions(): Promise<IExtension[]> {
-  return await localStorage.getItem(StorageItemKey.ExcludedExtensions)
+  return await storage.getItem(StorageItemKey.ExcludedExtensions)
 }
 
 export async function setExcludedExtensions(
   excludedExtensions: IExtension[]
 ): Promise<null> {
-  await localStorage.setItem(StorageItemKey.ExcludedExtensions, excludedExtensions)
+  await storage.setItem(StorageItemKey.ExcludedExtensions, excludedExtensions)
   return null
 }
